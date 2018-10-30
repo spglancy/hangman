01c867fb8cb35dbbc06a5b965daf470bb1348036
Sean <Spglancy@gmail.com>
2018-10-30 14:05:11 -0700

    working game

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,20 @@
 from random import randint
+import requests
 import sys
-words = ["placeholder", "death", "destruction", "cube"]
+words = ["placeholder"]
+tried = []
 displayString = ""
+
+def getWord():
+    word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+
+    response = requests.get(word_site)
+    WORDS = response.content.splitlines()
+
+    word = (WORDS[randint(0,len(WORDS)-1)])
+    word = str(word)
+    return(word[2:-1].lower())
+# credit to Ki for this ^^^
 
 def stringChooser():
     answerInternal =  words[randint(0,len(words)-1)]
@@ -16,28 +29,36 @@ def strReplace(str,index,char):
 def checker(key):
     global displayString
     for letter in displayString:
-        sys.stdout.write(letter+" ")
+        sys.stdout.write(letter + " ")
         sys.stdout.flush()
     print("")
     x = input("input a letter: ").lower()
-    i = 0
-    i = key.find(x)
-    if (i==-1):
-        return failCount()
-    ansCopy = key
-    while(i!=-1):
-        displayString = strReplace(displayString, i, key[i])
-        i = ansCopy.find(x)
-        ansCopy = ansCopy[:i]
+    if (x == key):
+        print(key)
+        print("You Win")
+        sys.exit()
+    elif (len(x) > 1):
+        print("Invalid input, please try again")
+        return checker(key)
+    if (key.find(x) == -1):
+        if (x not in tried):
+            tried.append(x)
+            print(tried)
+        else:
+            print("You already tried that letter!")
+        if(len(tried) == 7):
+            print("You Lose")
+            print("The string was: " + key)
+            sys.exit()
+    for i in range(len(displayString)):
+        if (key[i] == x):
+            displayString = strReplace(displayString, i, key[i])
+    if("_" not in displayString):
+        print(key)
+        print("You Win")
+        sys.exit()
     return checker(key)
 
-def failCount():
-    fail = 0
-    fail+=1
-    if(fail==7):
-        return "YOU FAILED"
-    checker(key)
-
-key = stringChooser()
+key = getWord()
 displayString = str("_"*len(key))
 checker(key)
